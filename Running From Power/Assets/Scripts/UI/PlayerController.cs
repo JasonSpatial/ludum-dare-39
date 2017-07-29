4e3af69dc18d2ba798764044360c58f6190aea89
Michael Grand <michael.grand.2@gmail.com>
2017-07-29 16:45:16 -0700

    The player can now jump.
    - Ground collisions added to the "industrial" tileset.
    - Default physics material set to Frictionless.
    - Tiled object types XML added to Maps directory. Import this into Tiled to
      access the "Ground" type.
    - The player can now jump by pressing Fire1. The player will only jump if they are on the ground.

---
+++ Running From Power/Assets/Scripts/UI/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿namespace Assets.Scripts.UI
+{
+    using UnityEngine;
+    using UnityEngine.Assertions;
+
+    /// <summary>
+    ///     Handles controlling the player.
+    /// </summary>
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        private float jumpImpulse = 10;
+
+        [SerializeField]
+        private bool grounded = false;
+
+        private int groundIntersectionsCount = 0;
+
+        private Rigidbody2D physicsBody;
+
+        private void Jump()
+        {
+            physicsBody.AddForce(jumpImpulse * Vector2.up, ForceMode2D.Impulse);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            groundIntersectionsCount++;
+            grounded = true;
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            groundIntersectionsCount--;
+            Assert.IsTrue(groundIntersectionsCount >= 0, "Ground intersections counter has become negative!");
+            if (groundIntersectionsCount == 0)
+            {
+                grounded = false;
+            }
+        }
+
+        private void Start()
+        {
+            physicsBody = GetComponent<Rigidbody2D>();
+            Assert.IsTrue(physicsBody != null, "PlayerController requires a RigidBody2D component and there was none.");
+        }
+
+        private void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if (grounded) Jump();
+            }
+        }
+    }
+}
--- Running From Power/Tilesets/industrial.tsx
+++ Running From Power/Tilesets/industrial.tsx
@@ -3,22 +3,26 @@
  <image source="platformerPack_industrial_tilesheet.png" width="980" height="560"/>
  <tile id="0">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
+   <object id="1" type="Ground" x="0" y="0" width="70" height="17.5"/>
+   <object id="2" x="0" y="17.5" width="70" height="52.5"/>
   </objectgroup>
  </tile>
  <tile id="1">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
+   <object id="1" x="0" y="17.5" width="70" height="52.5"/>
+   <object id="3" type="Ground" x="0" y="0" width="70" height="17.5"/>
   </objectgroup>
  </tile>
  <tile id="2">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
+   <object id="1" type="Ground" x="0" y="0" width="70" height="17.5"/>
+   <object id="2" x="0" y="17.5" width="70" height="52.5"/>
   </objectgroup>
  </tile>
  <tile id="3">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
+   <object id="1" x="0" y="17.5" width="70" height="52.5"/>
+   <object id="2" type="Ground" x="0" y="0" width="70" height="17.5"/>
   </objectgroup>
  </tile>
  <tile id="4">
@@ -28,45 +32,52 @@
  </tile>
  <tile id="7">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 70,0 70,43.75 0,43.75"/>
+   <object id="1" x="0" y="17.5">
+    <polygon points="0,0 70,0 70,26.25 0,26.25"/>
    </object>
+   <object id="2" type="Ground" x="0" y="0" width="70" height="17.5"/>
   </objectgroup>
  </tile>
  <tile id="8">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 70,0 70,43.75 0,43.75"/>
+   <object id="1" x="0" y="17.5">
+    <polygon points="0,0 70,0 70,26.25 0,26.25"/>
    </object>
+   <object id="2" type="Ground" x="0" y="0" width="70" height="17.5"/>
   </objectgroup>
  </tile>
  <tile id="14">
   <objectgroup draworder="index">
-   <object id="1" x="70" y="0">
-    <polygon points="0,0 -70,0 -70,26.25 0,70"/>
+   <object id="1" x="70" y="17.5">
+    <polygon points="0,0 -70,0 -70,8.75 0,52.5"/>
    </object>
+   <object id="2" type="Ground" x="0" y="0" width="70" height="17.5"/>
   </objectgroup>
  </tile>
  <tile id="15">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 70,0 70,26.25 0,70"/>
+   <object id="1" x="0" y="17.5">
+    <polygon points="0,0 70,0 70,8.75 0,52.5"/>
    </object>
+   <object id="3" type="Ground" x="0" y="0" width="70" height="17.5"/>
   </objectgroup>
  </tile>
  <tile id="16">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
+   <object id="1" x="0" y="17.5" width="70" height="52.5"/>
+   <object id="2" type="Ground" x="0" y="0" width="70" height="17.5"/>
   </objectgroup>
  </tile>
  <tile id="17">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
+   <object id="1" x="0" y="17.5" width="70" height="52.5"/>
+   <object id="2" type="Ground" x="0" y="0" width="70" height="17.5"/>
   </objectgroup>
  </tile>
  <tile id="18">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
+   <object id="1" x="0" y="17.5" width="70" height="52.5"/>
+   <object id="2" type="Ground" x="0" y="0" width="70" height="17.5"/>
   </objectgroup>
  </tile>
  <tile id="19">
@@ -81,46 +92,54 @@
  </tile>
  <tile id="21">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 70,0 70,43.75 0,43.75"/>
+   <object id="1" x="0" y="17.5">
+    <polygon points="0,0 70,0 70,26.25 0,26.25"/>
    </object>
+   <object id="3" type="Ground" x="0" y="0" width="70" height="17.5"/>
   </objectgroup>
  </tile>
  <tile id="22">
   <objectgroup draworder="index">
-   <object id="2" x="0" y="0">
-    <polygon points="0,0 70,0 70,43.75 0,43.75"/>
+   <object id="2" x="0" y="17.5">
+    <polygon points="0,0 70,0 70,26.25 0,26.25"/>
    </object>
+   <object id="3" type="Ground" x="0" y="0" width="70" height="17.5"/>
   </objectgroup>
  </tile>
  <tile id="28">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
+   <object id="1" x="0" y="17.5" width="70" height="52.5"/>
+   <object id="2" type="Ground" x="0" y="0" width="70" height="17.5"/>
   </objectgroup>
  </tile>
  <tile id="29">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
+   <object id="1" x="0" y="17.5" width="70" height="52.5"/>
+   <object id="2" type="Ground" x="0" y="0" width="70" height="17.5"/>
   </objectgroup>
  </tile>
  <tile id="42">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
+   <object id="1" x="0" y="17.5" width="70" height="52.5"/>
+   <object id="2" type="Ground" x="0" y="0" width="70" height="17.5"/>
   </objectgroup>
  </tile>
  <tile id="43">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
+   <object id="1" x="0" y="17.5" width="70" height="52.5"/>
+   <object id="2" type="Ground" x="0" y="0" width="70" height="17.5"/>
   </objectgroup>
  </tile>
  <tile id="56">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
+   <object id="1" x="0" y="17.5" width="70" height="52.5"/>
+   <object id="2" type="Ground" x="0" y="0" width="70" height="17.5"/>
   </objectgroup>
  </tile>
  <tile id="57">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
+   <object id="1" x="0" y="17.5" width="70" height="52.5"/>
+   <object id="2" type="Ground" x="0" y="0" width="70" height="17.5"/>
   </objectgroup>
  </tile>
 </tileset>
