0df6d5e65d129d7abdd90943450c262236d7cfca
Michael Grand <michael.grand.2@gmail.com>
2017-07-29 12:36:58 -0700

    The map moves left and loops indefinitely.
    - Test map height reduced.
    - Scrolling background script added.

---
+++ Running From Power/Assets/Scripts/Infrastructure/ScrollingBackground.cs
@@ -0,0 +1,78 @@
+﻿namespace Assets.Scripts.Infrastructure
+{
+    using System.Collections.Generic;
+    using Tiled2Unity;
+    using UnityEngine;
+
+    /// <summary>
+    ///     Manages a scrolling background in the horizontal direction.
+    /// </summary>
+    public class ScrollingBackground : MonoBehaviour
+    {
+        [SerializeField]
+        private float speed = 10;
+
+        [SerializeField]
+        private List<TiledMap> parts;
+
+        [SerializeField]
+        private int nextPart = 0;
+
+        private TiledMap back;
+
+        private TiledMap front;
+
+        private Camera mainCamera;
+
+        private float CameraHalfWidth
+        {
+            get { if (mainCamera == null) return 0; return mainCamera.orthographicSize*mainCamera.aspect;  }
+        }
+
+		private TiledMap LoadNextMapPart(float baseX)
+        {
+            if (parts.Count == 0) return null;
+
+            if (nextPart >= parts.Count)
+            {
+                nextPart = 0;
+            }
+            else if (nextPart < 0)
+            {
+                nextPart = 0;
+            }
+
+            TiledMap part = parts[nextPart];
+            nextPart++;
+            if (nextPart >= parts.Count)
+            {
+                nextPart = 0;
+            }
+
+            return Instantiate(part, new Vector3(baseX, part.GetMapHeightInPixelsScaled()/2), Quaternion.identity, transform);
+        }
+
+        private void FixedUpdate()
+        {
+            front.transform.position += new Vector3(-speed*Time.deltaTime, 0);
+
+            back.transform.position = front.transform.position + new Vector3(front.GetMapWidthInPixelsScaled(), 0);
+
+            if (front.transform.position.x + front.GetMapWidthInPixelsScaled() <= -CameraHalfWidth)
+            {
+                front.transform.position = back.transform.position + new Vector3(back.GetMapWidthInPixelsScaled(), 0);
+                Destroy(front.gameObject);
+                front = back;
+                back = LoadNextMapPart(front.transform.position.x + front.GetMapWidthInPixelsScaled());
+            }
+        }
+
+        private void Start()
+        {
+            mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
+            front = LoadNextMapPart(CameraHalfWidth);
+            back = LoadNextMapPart(front.transform.position.x + front.GetMapWidthInPixelsScaled());
+        }
+    }
+}
