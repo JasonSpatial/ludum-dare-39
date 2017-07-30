78afd28589ade2dc1394314383b22ce011e209a1
Michael Grand <michael.grand.2@gmail.com>
2017-07-29 19:48:39 -0700

    The game resets if you fall into a pit.

---
+++ Running From Power/Assets/Scripts/Behaviors/TheVoid.cs
@@ -0,0 +1,16 @@
+﻿namespace Assets.Scripts.Behaviors
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    /// <summary>
+    ///     Handles ending the game if you fall into a pit.
+    /// </summary>
+    public class TheVoid : MonoBehaviour
+    {
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
